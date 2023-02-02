77cd3e4973f26d1a13f2eff9ace3638e361d63b1
TehnoTheDragon <tehnothedragon@gmail.com>
2023-02-03 00:03:56 +0200

    CraftItem class

---
+++ mods/fabox_core/src/CraftItem.lua
@@ -0,0 +1,46 @@
+local CraftItem = class "CraftItem" {}
+
+function CraftItem:_init(name)
+    self._name = fabox.name(name)
+    self.definition = {
+        groups = {}
+    }
+end
+
+-- Pre-Register Functions
+
+function CraftItem:onUse(func)
+    self.definition.on_use = func
+    return self
+end
+
+function CraftItem:image(image)
+    self.definition.inventory_image = image
+    return self
+end
+
+function CraftItem:description(text)
+    self.definition.description = text
+    return self
+end
+
+function CraftItem:group(name)
+    self.definition.groups[name] = 1
+    return self
+end
+
+-- Post Functions
+
+function CraftItem:identity()
+    return minetest.registered_craftitems[self._name]
+end
+
+function CraftItem:alias(name)
+    minetest.register_alias(name, self._name)
+end
+
+function CraftItem:register()
+    minetest.register_craftitem(self._name, self.definition)
+end
+
+return CraftItem
